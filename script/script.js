ce624eb36f1b05f46090df16ce1a0e61398a8833
Fedorovaks <ksyunya-fedorova@bk.ru>
2023-10-06 19:18:18 +0400

    завершенное

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var activForm = document.querySelector(".registration");
+var knopka = document.querySelector("#button");
+
+knopka.onclick = function() {
+    
+    activForm.style.display='flex';
+
+    };
+    
